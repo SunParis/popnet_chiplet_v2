b2b2afc3899e3f202296679fc455efe0f4a55289
SunParis <p1108sun@hotmail.com>
2025-05-06 23:00:22 +0800

    Update logger.hpp

--- includes/logger/logger.hpp
+++ includes/logger/logger.hpp
@@ -8,7 +8,10 @@
 # ifndef _LOGGER_HPP_
 # define _LOGGER_HPP_ 1
 
-# define LOGGER_OUT_DEFAULT std::string("stdout")
+# define _LOGGER_OUT_DEFAULT_ std::string("stdout")
+# define _GREEN_ fg(fmt::rgb(165, 210, 165))
+# define _ORANGE_ fg(fmt::rgb(230, 165, 105))
+# define _RED_ fg(fmt::rgb(250, 110, 110))
 
 # include <string>
 # include <iostream>
@@ -17,12 +20,101 @@
 # include <ctime>
 # include <iomanip>
 # include <sstream>
-# include <any>
-
-# include "global_defines/defines.h"
 
 # include "fmt/core.h"
 # include "fmt/color.h"
+
+# ifdef LOCK_ON
+
+# include <mutex>
+# include <atomic>
+# include <linux/futex.h>
+# include <sys/syscall.h>
+# include <unistd.h> // Add this include for syscall declaration
+
+namespace Logger {
+
+static int futex_wait(std::atomic_bool* addr, int expected) {
+    return syscall(SYS_futex, addr, FUTEX_WAIT, expected, nullptr, nullptr, 0);
+}
+
+static int futex_wake(std::atomic_bool* addr, int count) {
+    return syscall(SYS_futex, addr, FUTEX_WAKE, count, nullptr, nullptr, 0);
+}
+
+/**
+ * @brief A futex lock implementation.
+ * @note This class uses atomic operations and futex system calls to implement a lock. 
+ */
+class futex {
+    
+    /**
+     * @brief The state of the futex lock.
+     * @note This is an atomic boolean that indicates whether the lock is held (true) or not (false).
+     */
+    std::atomic_bool state{false};
+
+public:
+
+    /**
+     * @brief Default constructor.
+     */
+    futex() = default;
+    
+    /**
+     * @brief Deleted copy constructor.
+     */
+    futex(const futex&) = delete;
+
+    /**
+     * @brief Default destructor.
+     */
+    ~futex() = default;
+    
+    /**
+     * @brief Deleted copy assignment operator.
+     */
+    futex& operator=(const futex&) = delete;
+    
+    /**
+     * @brief Acquire the futex lock.
+     * @note This function will block until the lock is acquired.
+     */
+    void lock() {
+        bool expected = false;
+        // Try to acquire the lock
+        while (!this->state.compare_exchange_weak(expected, true, std::memory_order_acquire)) {
+            // The lock is already held, call futex to wait
+            futex_wait(&this->state, true);
+            expected = false;
+        }
+    }
+
+    /**
+     * @brief Release the futex lock.
+     * @note This function will wake one waiting thread.
+     */
+    void unlock() {
+        this->state.store(false, std::memory_order_release);
+        // Wake one waiting thread
+        futex_wake(&this->state, true);
+    }
+
+    /**
+     * @brief Try to acquire the futex lock without blocking.
+     * @return true if the lock was acquired, false otherwise.
+     */
+    bool try_lock() {
+        bool expected = false;
+        // Try to acquire the lock
+        return this->state.compare_exchange_strong(expected, true, std::memory_order_acquire);
+    }
+
+};
+
+};
+
+# endif
 
 enum class LogLevel: unsigned char {
     Debug = 0,
@@ -49,13 +141,25 @@ namespace Logger {
     }
 
     inline LogLevel CURR_LOGLEVEL = LogLevel::Info;
-    inline std::string LOGGER_OUT = LOGGER_OUT_DEFAULT;
+    inline std::string LOGGER_OUT = _LOGGER_OUT_DEFAULT_;
+
+# ifdef LOCK_ON
+
+    inline Logger::futex LOGGER_LOCK;
+    inline Logger::futex LOGLEVEL_LOCK;
+    inline Logger::futex LOGGER_OUT_LOCK;
+
+# endif
+
 
     /**
      * @brief Set the log level
      * @param level The log level
      */
     inline void setLogLevel(LogLevel level) {
+# ifdef LOCK_ON
+        const std::lock_guard<Logger::futex> lock(Logger::LOGLEVEL_LOCK);
+# endif
         Logger::CURR_LOGLEVEL = level;
     }
 
@@ -64,6 +168,9 @@ namespace Logger {
      * @return The log level
      */
     inline LogLevel getLogLevel() {
+# ifdef LOCK_ON
+        const std::lock_guard<Logger::futex> lock(Logger::LOGLEVEL_LOCK);
+# endif
         return Logger::CURR_LOGLEVEL;
     }
 
@@ -74,6 +181,9 @@ namespace Logger {
      * @note The default output is std::cout
      */
     inline void setLoggerOut(const std::string& out) {
+# ifdef LOCK_ON
+        const std::lock_guard<Logger::futex> lock(Logger::LOGGER_OUT_LOCK);
+# endif
         Logger::LOGGER_OUT = out;
     }
 
@@ -82,6 +192,9 @@ namespace Logger {
      * @return The logger output
      */
     inline std::string getLoggerOut() {
+# ifdef LOCK_ON
+        const std::lock_guard<Logger::futex> lock(Logger::LOGGER_OUT_LOCK);
+# endif
         return Logger::LOGGER_OUT;
     }
 
@@ -93,35 +206,44 @@ namespace Logger {
      */
     template<typename... Args>
     inline void log(LogLevel level, fmt::format_string<Args...> fmtstr, Args&&... args) {        
-        std::string timeStr = Logger::getCurrentTime();        
-        
-        if (Logger::LOGGER_OUT == "std::cout" || Logger::LOGGER_OUT == "stdout") {
-            
+        std::string timeStr = Logger::getCurrentTime();
+
+        if (Logger::getLoggerOut() == "std::cout" || Logger::getLoggerOut() == "stdout") {
+# ifdef LOCK_ON
+            const std::lock_guard<Logger::futex> lock(Logger::LOGGER_LOCK);
+# endif
+            fmt::print("[{}][", timeStr);
             switch (level) {
                 case LogLevel::Info: 
-                    fmt::print(fmt::fg(fmt::color::green), "[{}][info ] ", timeStr);
+                    fmt::print(_GREEN_, "info ");
                     break;
                 case LogLevel::Debug: 
-                    fmt::print(fmt::fg(fmt::color::green), "[{}][debug] ", timeStr);
+                    fmt::print(_GREEN_, "debug");
                     break;
                 case LogLevel::Warn:
-                    fmt::print(fmt::fg(fmt::color::orange), "[{}][warn ] ", timeStr);
+                    fmt::print(_ORANGE_, "warn ");
                     break;
                 case LogLevel::Error:
-                    fmt::print(fmt::fg(fmt::color::red), "[{}][error] ", timeStr);
+                    fmt::print(_RED_, "error");
                     break;
             }
 
             fmt::print("\033[0m");
+            fmt::print("] ");
             fmt::print(fmtstr, std::forward<Args>(args)...);
             fmt::print("\n");
         }
         else {
-            std::ofstream logFile(Logger::LOGGER_OUT, std::ios::app);
+            std::ofstream logFile(Logger::getLoggerOut(), std::ios::app);
+# ifdef LOCK_ON
+            const std::lock_guard<Logger::futex> lock(Logger::LOGGER_LOCK);
+# endif
             if (!logFile.is_open()) {
-                fmt::print(fmt::fg(fmt::color::red), "[{}][error] ", timeStr);
+                fmt::print("[{}][");
+                fmt::print(_RED_, "error");
                 fmt::print("\033[0m");
-                fmt::print("Could not open log file: {}\n", Logger::LOGGER_OUT);
+                fmt::print("] ");
+                fmt::print("Could not open log file: {}\n", Logger::getLoggerOut());
                 return;
             }            
             std::string out_str = "";
@@ -151,7 +273,7 @@ namespace Logger {
      */
     template<typename... Args>
     inline void debug(fmt::format_string<Args...> fmtstr, Args&&... args) {
-        if (Logger::CURR_LOGLEVEL > LogLevel::Debug) {
+        if (Logger::getLogLevel() > LogLevel::Debug) {
             return;
         }
         Logger::log(LogLevel::Debug, fmtstr, std::forward<Args>(args)...);
@@ -162,7 +284,7 @@ namespace Logger {
      */
     template<typename... Args>
     inline void info(fmt::format_string<Args...> fmtstr, Args&&... args) {
-        if (Logger::CURR_LOGLEVEL > LogLevel::Info) {
+        if (Logger::getLogLevel() > LogLevel::Info) {
             return;
         }
         Logger::log(LogLevel::Info, fmtstr, std::forward<Args>(args)...);
@@ -173,7 +295,7 @@ namespace Logger {
      */
     template<typename... Args>
     inline void warn(fmt::format_string<Args...> fmtstr, Args&&... args) {
-        if (Logger::CURR_LOGLEVEL > LogLevel::Warn) {
+        if (Logger::getLogLevel() > LogLevel::Warn) {
             return;
         }
         Logger::log(LogLevel::Warn, fmtstr, std::forward<Args>(args)...);
@@ -184,7 +306,7 @@ namespace Logger {
      */
     template<typename... Args>
     inline void error(fmt::format_string<Args...> fmtstr, Args&&... args) {
-        if (Logger::CURR_LOGLEVEL > LogLevel::Error) {
+        if (Logger::getLogLevel() > LogLevel::Error) {
             return;
         }
         Logger::log(LogLevel::Error, fmtstr, std::forward<Args>(args)...);
@@ -198,7 +320,7 @@ namespace Logger {
      */
     template<typename T>
     inline std::string stream_to_string(const T& args) {
-        std::stringstream ss;
+        std::ostringstream ss;
         ss << args;
         return ss.str();
     }
